a155b7138637d34cf019ac700f539db82f21c88a
Dev07 <dev07@Dev07s-MacBook-Pro.local>
2018-12-18 14:57:05 +0200

    setup socketIO

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+var http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
+io.on('connection',(socket)=>{
+    console.log('You are connnected');
+    socket.on('disconnect',()=>{
+        console.log('You are disconnected');
+    })
+})
+
+
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
