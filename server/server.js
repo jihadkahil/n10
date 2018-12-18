61c63205962f0cb71b2dcb4294e74e5d6bf66179
Dev07 <dev07@192.168.0.102>
2018-12-18 22:02:29 +0200

    socketIo Brodcast and

--- server/server.js
+++ server/server.js
@@ -1,26 +1,52 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-var http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-
 var server = http.createServer(app);
 var io = socketIO(server);
 
-
-io.on('connection',(socket)=>{
-    console.log('You are connnected');
-    socket.on('disconnect',()=>{
-        console.log('You are disconnected');
-    })
-})
-
-
-
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+ 
+
+    //after user get it's connection from server side
+    // the server will emit for the user this messages
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+
+  //socket.broadcast.emit ==> sent to all connected send success and the new user is connected
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  // recieve from clide
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text +"Brodcast",
+      createdAt: new Date().getTime()
+    });
+    
+  });
+
+  
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
